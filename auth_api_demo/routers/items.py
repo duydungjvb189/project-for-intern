88fc57b5b55f13ddf1d80a45e897c5586c5f58ff
duydungjvb189 <duydungjvb@gmail.com>
2025-10-28 13:31:02 +0700

    CRUD and Auth features
    Detail:
    - Registered with username and password
    - Logged in. Return access_token and refresh_token
    - Created new refresh_token when expired
    - Build a basic crud frame

---
+++ auth_api_demo/routers/items.py
@@ -0,0 +1,32 @@
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends
+from database import get_db
+from services.item_service import (
+    create_item_service,
+    get_item_by_id_service,
+    get_all_items_service,
+    update_item_service,
+    delete_item_service
+)
+
+router = APIRouter(prefix="/items", tags=["Items"])
+
+@router.post("/", status_code=201)
+def create_item(item_data: dict, db: Session = Depends(get_db)):
+    return create_item_service(db, item_data)
+
+@router.get("/{item_id}")
+def get_item(item_id: int, db: Session = Depends(get_db)):
+    return get_item_by_id_service(db, item_id)
+
+@router.get("/")
+def get_all_items(db: Session = Depends(get_db)):
+    return get_all_items_service(db)
+
+@router.put("/{item_id}")
+def update_item(item_id: int, item_data: dict, db: Session = Depends(get_db)):
+    return update_item_service(db, item_id, item_data)
+
+@router.delete("/{item_id}")
+def delete_item(item_id: int, db: Session = Depends(get_db)):
+    return delete_item_service(db, item_id)
---
+++ auth_api_demo/schemas/token_schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+class TokenData(BaseModel):
+    message: str
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+class RefreshTokenRequest(BaseModel):
+    refresh_token: str
+
+class RefreshTokenData(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
